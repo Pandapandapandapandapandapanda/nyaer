78a85c39ee321c9a83c6de8616742bfaa830ebdb
panda <161661232+Pandapandapandapandapandapanda@users.noreply.github.com>
2024-08-21 15:31:11 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+//Variables
+
+//Functions
+
+//Events
+document.addEventListener("DOMContentLoaded", function() {
+  
+  document.getElementById("login-button").addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("meow");
+  });
+  
+});
