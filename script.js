9252a971493248d05b25d545c8e77d81a6757ffa
panda <161661232+Pandapandapandapandapandapanda@users.noreply.github.com>
2024-08-22 15:17:21 -0700

    Login update
    You can now login via Nya-er

--- script.js
+++ script.js
@@ -1,13 +1,74 @@
 //Variables
+let cloudLink;
+let token;
+let server = "wss://server.meower.org";
+let api = "https://api.meower.org/"
 
 //Functions
+function onMessage(event){
+  const packet = JSON.parse(event.data);
+  console.log(packet);
+}
+
+function connectToWebSocket(){
+  cloudLink = new WebSocket(server);
+  cloudLink.onmessage = onMessage;
+  cloudLink.onopen = () => console.log("WebSocket connection opened.");
+  cloudLink.onerror = (error) => console.error("WebSocket error:", error);
+  return new Promise((resolve, reject) => {
+    cloudLink.addEventListener("open", () => { resolve()
+    })
+  })
+}
+
+function login(username, password){
+  return fetch(api+"auth/login", {
+    method: "POST",
+    headers: {"content-type": "application/json"},
+    body:JSON.stringify({
+      username: username,
+      password: password
+    })
+  }).then((response) => {
+    if (!response.ok)  {
+      console.error("Problem getting messages:", response.status);
+    }
+    return response.json();
+  })
+}
+
+async function cloudLinkLogin(username, password){
+  const tempToken = (await login(username, password))
+  if(tempToken.error){
+    console.warn(tempToken.type);
+    return false;
+  }
+  token = tempToken.token;
+  const authPacket = {
+    cmd: "direct",
+    val: {
+      cmd: "authpswd",
+      val: {username: username, pswd: token}
+    }
+  };
+  if(!(cloudLink && cloudLink.readyState !== WebSocket.OPEN)){
+    await connectToWebSocket();
+  }
+  cloudLink.send(JSON.stringify(authPacket));
+  return new Promise((resolve, reject) => {
+    cloudLink.addEventListener("message", (event) => {
+      if (JSON.parse(event.data).val == "I:100 | OK") {resolve()}
+    })
+  })
+}
 
 //Events
 document.addEventListener("DOMContentLoaded", function() {
-  
   document.getElementById("login-button").addEventListener("click", function(event){
     event.preventDefault();
     console.log("meow");
+    let username = document.getElementById("username-form-login-page").value;
+    let password = document.getElementById("password-form-login-page").value;
+    cloudLinkLogin(username, password);
   });
-  
 });
