a33e04be20450cdc262cf802a3063ce23e44936d
panda <161661232+Pandapandapandapandapandapanda@users.noreply.github.com>
2024-08-25 17:45:43 -0700

    Message sending
    (only works in home rn)

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Variables
 let cloudLink;
 let token;
+let currentChannel;
 let server = "wss://server.meower.org";
 let api = "https://api.meower.org/"
 
@@ -8,6 +9,10 @@ let api = "https://api.meower.org/"
 function onMessage(event){
   const packet = JSON.parse(event.data);
   console.log(packet);
+  if (packet.cmd=="direct" && packet.val.mode=='auth'){
+    token = packet.val.payload.token;
+    console.log(token);
+  }
 }
 
 function connectToWebSocket(){
@@ -76,12 +81,48 @@ async function doLogin(){
   }
   document.body.style.backdropFilter = "none";
   document.getElementById("login-page").style.display = "none";
+  document.getElementById("nyaer").style.display = "block";
+  currentChannel = "home";
+}
+
+function sendMessage(message, channel){
+  console.log(message, channel, token);
+  fetch(api+channel, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      token: token
+    },
+    body: JSON.stringify({
+      content: message
+    })
+  }).then(async response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Message sent successfully:', data);
+  })
+  .catch(error => {
+    console.error('There was a problem sending the message:', error);
+    console.log(error);
+  });
 }
 
 //Events
 document.addEventListener("DOMContentLoaded", function() {
+  
   document.getElementById("login-button").addEventListener("click", function(event){
     event.preventDefault();
     doLogin();
   });
+
+  document.getElementById("submit-button").addEventListener("click", function(event){
+    event.preventDefault();
+    let message = document.getElementById("main-input")
+    sendMessage(message.value, currentChannel);
+    message.value = "";
+  })
 });
